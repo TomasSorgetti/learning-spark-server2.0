1f62ab43bced456de194588b46ea925d5e5b1893
TomasSorgetti <tomassorgetti456@gmail.com>
2024-10-27 01:40:54 -0300

    probando husky

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const routes = express.Router();
 
 routes.use('/auth', require('./domains/auth/auth.routes'));
 routes.use('/user', require('./domains/user/user.routes'));
+// routes.use('/user', require('./domains/user/user.routes'));
 
 module.exports = routes;
